583a27abe71105f67913b805052206b6cb6e78b6
aakashdeveloper <ahanda205@gmail.com>
2022-02-21 08:48:07 +0530

    classComponent

---
+++ firstapp/src/component/Header.js
@@ -0,0 +1,28 @@
+import React,{Fragment,Component} from 'react'
+
+class Header extends Component {
+    render(){
+        return(
+            <Fragment>
+                <center>
+                    <h1>Developer Funnel</h1>
+                </center>
+                <hr/>
+            </Fragment>
+        ) 
+    }
+}
+
+
+/*
+const Header = () => {
+    return(
+        <Fragment>
+            <center>
+                <h1>Developer Funnel</h1>
+            </center>
+            <hr/>
+        </Fragment>
+    )
+}*/
+export default Header
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,24 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/*
-function Home(){
-  return(
-    <div>
-      <h1>Hiii From React Old</h1>
-      <h2>Hello</h2>
-    </div>
-  )
-}*/
-
-
-const Home = () => {
-  return(
-    <div>
-      <h1>Hiii From React Old</h1>
-      <h2>Hello</h2>
-    </div>
-  )
-}
-
+import Home from './component/Home'
 ReactDOM.render(<Home/>,document.getElementById('root'))
