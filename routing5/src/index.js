2e7be004a0b556ee3608b09ca6f5429f2f1f2e8f
aakashdeveloper <ahanda205@gmail.com>
2022-03-10 08:54:14 +0530

    routing5

--- routing5/src/index.js
+++ routing5/src/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Component/Header';
-import Footer from './Component/Footer';
-import Home from './Component/Home';
-import Post from './Component/Post';
-import Profile from './Component/Profile';
+import Routing from './Component/Routing';
+ReactDOM.render(<Routing/>, document.getElementById('root'))
